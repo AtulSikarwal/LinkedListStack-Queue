3cf9228d550d5c98fa9239850f281d5c4e970e0f
AtulSikarwal <atulsikarwal5.com>
2022-06-23 16:45:56 +0530

    The code for Create a Queue LinkedList

---
+++ CreateQueue/Queue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class Queue
+    {
+        Linkedlist linkedlist;
+
+        public Queue()
+        {
+            linkedlist = new Linkedlist();
+        }
+
+
+        public void Enqueue(Node node)
+        {
+            linkedlist.AppendNode(node);
+        }
+        public void DisplayQueue()
+        {
+            Console.WriteLine("Queue :");
+            linkedlist.Display();
+        }
+       
+    }
+}
