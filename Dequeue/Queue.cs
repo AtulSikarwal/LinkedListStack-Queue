6c4f3cae3347d7f55307d8b298adc4520a191fe5
AtulSikarwal <atulsikarwal5.com>
2022-06-23 16:49:53 +0530

    The code for Dequeue in the Queue in LinkedList

---
+++ Dequeue/Queue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class Queue
+    {
+        Linkedlist linkedlist;
+
+        public Queue()
+        {
+            linkedlist = new Linkedlist();
+        }
+
+
+        public void Enqueue(Node node)
+        {
+            linkedlist.AppendNode(node);
+        }
+        public void DisplayQueue()
+        {
+            Console.WriteLine("Queue :");
+            linkedlist.Display();
+        }
+
+
+        public void Top()
+        {
+            var val = linkedlist.Head;
+            Console.WriteLine(val.data);
+        }
+
+        public void Deenque()
+        {
+            linkedlist.DeleteNodeAtLast();
+        }
+
+    }
+}
